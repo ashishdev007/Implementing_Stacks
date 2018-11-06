43165a74ced225a821e9c55db1a9ce6b465298bd
ashishdev007 <ashishdevofficial@outlook.com>
2018-11-05 21:31:09 -0600

    Added a tester class to test the interface

---
+++ src/Tester.java
@@ -0,0 +1,11 @@
+public class Tester {
+    public static void main(String args[]){
+        StackInt<String> stack = new ArrayImplement<>();
+
+        stack.push("a");
+        stack.push("b");
+        stack.push("c");
+
+    }
+
+}
