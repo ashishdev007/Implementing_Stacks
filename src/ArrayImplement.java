cd818848961945d84bf807aec4c8de6ec463bee1
ashishdev007 <ashishdevofficial@outlook.com>
2018-11-05 20:44:18 -0600

    Added StackInt interface

---
+++ src/ArrayImplement.java
@@ -0,0 +1,2 @@
+public class ArrayImplement implements StackInt {
+}
---
+++ src/StackInt.java
@@ -0,0 +1,11 @@
+public interface StackInt<E> {
+
+     E push(E obj);
+
+     E peek();
+
+     E pop();
+
+     boolean empty();
+
+}
