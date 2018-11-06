8f77a8e0462bf083e155e56ec370cf6ae6b25f4c
ashishdev007 <ashishdevofficial@outlook.com>
2018-11-05 21:20:52 -0600

    Added the constructor and empty() method

--- src/ArrayImplement.java
+++ src/ArrayImplement.java
@@ -1,2 +1,31 @@
-public class ArrayImplement implements StackInt {
+import java.util.ArrayList;
+
+public class ArrayImplement<E> implements StackInt<E> {
+
+    private ArrayList<E> list;
+
+    public ArrayImplement(){
+        list = new ArrayList<>();
+    }
+
+    public E push(E obje){
+        return (E) "as";
+    }
+
+    @Override
+    public E peek() {
+        return null;
+    }
+
+    public E pop(){
+       return (E) "Ssf";
+   }
+
+    @Override
+    public boolean empty() {
+        if (list.size() == 0){
+            return true;
+        }
+        return false;
+    }
 }
