98d94a5c296e59dd663a60b830a2458be2c3e97d
ashishdev007 <ashishdevofficial@outlook.com>
2018-11-06 19:14:39 -0600

    "Modified tester to check the new implementation."

--- src/Tester.java
+++ src/Tester.java
@@ -1,24 +1,17 @@
 public class Tester {
     public static void main(String args[]){
-        StackInt<String> stack = new ArrayImplement<>();
+        StackInt<String> stack = new LinkedStack<>();
 
         stack.push("a");
         stack.push("b");
         stack.push("c");
 
-        System.out.println(stack);
-        System.out.println(stack.pop());
-        System.out.println(stack);
-        System.out.println(stack.pop());
-        System.out.println(stack);
-        System.out.println(stack.pop());
+        stack.pop();
+        stack.pop();
+        stack.pop();
+        stack.pop();
 
-        try {
-            System.out.println(stack);
-            System.out.println(stack.peek());
-        } catch (Exception e) {
-            System.out.println("Error");
-        }
+        System.out.println(stack);
     }
 
 }
